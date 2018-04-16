9ce9f49b33bf247512b2878905847d17099e845e
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2018-04-16 12:13:59 +0300

    2. fix Jdbc tests. Validation was excluded. For JdbcMealServiceTest added beforeClass validation to check active profile

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -1,10 +1,18 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Assume;
+import org.junit.BeforeClass;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
+
+    @BeforeClass
+    public static void check() {
+        Assume.assumeTrue(Profiles.REPOSITORY_IMPLEMENTATION == Profiles.JDBC);
+    }
 }
