d522151af7c8fbbcfe55373f28dcb1e0370f21c3
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2018-04-29 19:19:37 +0300

    1. Implemented meals via Datatables js and new MealAjaxController. Used Bootstrap. Used AJAX.

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -0,0 +1,66 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealWithExceed;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = MealAjaxController.REST_URL)
+public class MealAjaxController extends AbstractMealController {
+    static final String REST_URL = "/ajax/profile/meals";
+
+    @Override
+    @GetMapping("/{id}")
+    public Meal get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealWithExceed> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Meal meal, @PathVariable("id") int id) {
+        super.update(meal, id);
+    }
+
+
+    @PostMapping
+    public void createWithLocation(@RequestParam("id") Integer id,
+                                   @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
+                                   @RequestParam("description") String description,
+                                   @RequestParam("calories") Integer calories) {
+        Meal meal = new Meal(id, dateTime, description, calories);
+        if (meal.isNew()) {
+            super.create(meal);
+        }
+    }
+
+    @Override
+    @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealWithExceed> getBetween(
+            @RequestParam(value = "startDate", required = false) LocalDate startDate,
+            @RequestParam(value = "startTime", required = false) LocalTime startTime,
+            @RequestParam(value = "endDate", required = false) LocalDate endDate,
+            @RequestParam(value = "endTime", required = false) LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
+}
