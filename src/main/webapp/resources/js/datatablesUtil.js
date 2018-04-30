3f9de16a9ae33d119de603e372ba621d7e598d5a
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2018-04-30 10:24:46 +0300

    2. make html id attribute unique by moving it to row level.

--- src/main/webapp/resources/js/datatablesUtil.js
+++ src/main/webapp/resources/js/datatablesUtil.js
@@ -1,6 +1,6 @@
 function makeEditable() {
     $(".delete").click(function () {
-        deleteRow($(this).attr("id"));
+        deleteRow($(this).parent().parent().attr("id"));
     });
 
     $(document).ajaxError(function (event, jqXHR, options, jsExc) {
